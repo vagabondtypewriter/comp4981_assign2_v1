18978a21173ac3ea6906115645d88588308c4c54
vagabondtypewriter <kieferthominquiries@gmail.com>
2024-03-18 16:39:09 -0700

    add fix script (need to recopy the scripts from the template)

---
+++ fix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ./build ./build-all.sh ./build.sh ./change-compiler ./change-compiler.sh ./check-compilers.sh ./check-env.sh
+rm -rf ./.clang-format ./CMakeLists.txt ./.flags ./generate-cmakelists.sh ./generate-flags.sh ./gmon.out ./supported_c_compilers.txt
